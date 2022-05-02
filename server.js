3a5f904d64de80ebb8e784b3632f55e718085604
Loick Hommet <hommet.loick@gmail.com>
2022-05-02 16:00:50 +0200

    Ajoue de la condition get pour affichier le text hello world

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-    
-    if (req.url == "/"){ 
-            res.writeHead(200, { "content-type": "text/html" });
-            res.write("<h1>Home World Loick</h2>");
-        }
-        
-    
-})
+  if (req.url == "/") {
+    if (req.method == "GET") {
+      res.writeHead(200, { "content-type": "text/html" });
+      res.write("<h1>Home World Loick</h2>");
+    }
+  }
+});
 
-server.listen(4000)
+server.listen(4000);
