e5454f11bc819d0d8afe38e7c6bc96726839d614
Loick Hommet <hommet.loick@gmail.com>
2022-05-02 15:14:48 +0200

    Ajoue d'un text Hello wold loick sur ma route /

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const http = require("http")
+const http = require("http");
 
 const server = http.createServer((req, res) => {
-
-
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>Home World Loick</h2>");
     res.end()
 })
 
-server.listen(5000)
+server.listen(4000)
