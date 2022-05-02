e1e10d18fa9491687825acc0eb711cd8d89fe3ce
Loick Hommet <hommet.loick@gmail.com>
2022-05-02 16:02:57 +0200

    Ajoue de la condition 401 si l'utilisateur va sur la route / avec une autre method que GET

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const server = http.createServer((req, res) => {
     if (req.method == "GET") {
       res.writeHead(200, { "content-type": "text/html" });
       res.write("<h1>Home World Loick</h2>");
+    }else{
+        res.writeHead(401, { "content-type": "text/html" });
+        res.write("<h1>401 Méthode non authorisée</h2>"); 
     }
   }
 });
