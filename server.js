d4c02cd088d274459e6fa347c1014a77c9d3b494
Loick Hommet <hommet.loick@gmail.com>
2022-05-02 15:57:54 +0200

    Ajoue de la condition / pour afficher le text précedant

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>Home World Loick</h2>");
-    res.end()
+    
+    if (req.url == "/"){ 
+            res.writeHead(200, { "content-type": "text/html" });
+            res.write("<h1>Home World Loick</h2>");
+        }
+        
+    }
 })
 
 server.listen(4000)
