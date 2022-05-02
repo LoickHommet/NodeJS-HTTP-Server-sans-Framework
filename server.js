39a91c6beb34e1cfa093a296469b75df27c06008
Loick Hommet <hommet.loick@gmail.com>
2022-05-02 15:41:22 +0200

    Ajoute d'une condition de la route / pour afficher le text hello world

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const http = require("http");
+const { url, router } = require("inspector");
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>Home World Loick</h2>");
+
+    console.log(req.method)
+
+    if (req.url == "/"){
+        res.writeHead(200, { "content-type": "text/html" });
+        res.write("<h1>Home World Loick</h2>");
+    }
+  
     res.end()
 })
 
